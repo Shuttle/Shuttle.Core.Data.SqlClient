f265e2dccac150dfa94344e340cf9c985b3a2725
eben-roux <me@ebenroux.co.za>
2018-01-01 09:59:38 +0200

    - initial commit

---
+++ Shuttle.Core.Data.SqlClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if NETCOREAPP2_0
+[assembly: AssemblyTitle(".NET Core 2.0")]
+#endif
+
+#if NETSTANDARD2_0
+[assembly: AssemblyTitle(".NET Standard 2.0")]
+#endif
+
+[assembly: AssemblyVersion("10.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
+[assembly: AssemblyProduct("Shuttle.Core.Data.SqlClient")]
+[assembly: AssemblyCompany("Shuttle")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyInformationalVersion("10.0.0")]
+[assembly: ComVisible(false)]
