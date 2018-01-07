c55555048f05101060b5e344074d6eed364af0a8
eben-roux <me@ebenroux.co.za>
2018-01-07 09:11:37 +0200

    - added bootstrapping

---
+++ Shuttle.Core.Data.SqlClient/Bootstrap.cs
@@ -0,0 +1,15 @@
+﻿using Shuttle.Core.Container;
+using Shuttle.Core.Contract;
+
+namespace Shuttle.Core.Data.SqlClient
+{
+    public class Bootstrap : IComponentRegistryBootstrap
+    {
+        public void Register(IComponentRegistry registry)
+        {
+            Guard.AgainstNull(registry, nameof(registry));
+
+            registry.AttemptRegister<IDbProviderFactories, DbProviderFactories>();
+        }
+    }
+}
